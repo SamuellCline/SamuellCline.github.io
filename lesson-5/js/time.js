2429c5f4be731b8ce43cdb25fe46ae42fa98a8c4
Samuel Cline <SamuellCline@gmail.com>
2017-10-20 23:14:01 -0600

    added responsive image and date

--- lesson-5/js/time.js
+++ lesson-5/js/time.js
@@ -1,1 +1,16 @@
+var a = new Date();
+var weekday = new Array(7);
+weekday[0] = "Sunday";
+weekday[1] = "Monday";
+weekday[2] = "Tuesday";
+weekday[3] = "Wednesday";
+weekday[4] = "Thursday";
+weekday[5] = "Friday";
+weekday[6] = "Saturday";
+
+var d = weekday[a.getDay()];
+var d1 = a.getDate
+var m = a.getMonth;
+var y = a.getYear;
+var date = d + d1 + m + y;
 document.getElementById("date").innerHTML = Date();
