17f166b045adedc91b4bb082457d76c229038db6
Samuel Cline <SamuellCline@gmail.com>
2017-11-11 18:28:48 -0700

    Added Improved Thank you Screen

---
+++ lesson-9/storm/js/thanks1.js
@@ -0,0 +1,6 @@
+var canvas1 = document.getElementById("canvas");
+var ctx = canvas1.getContext("2d");
+ctx.font = "30px Comic Sans MS";
+ctx.fillStyle = "red";
+ctx.textAlign = "center";
+ctx.fillText("Hello World");
